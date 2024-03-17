bb7a7ee0961ee4bce60a62bf008deae0eb8e2042
Flavian Stavarache <FlavianStavarache@outlook.com>
2024-03-17 17:53:42 +0100

    added velocity Verlet integrator

--- src/MolecularDynamics.jl
+++ src/MolecularDynamics.jl
@@ -167,7 +167,7 @@ function lj_force(sigma, epsilon, r, d)
     fraction = sigma / r
     fraction7 = fraction^7
 
-	return -24 * epsilon * d * (2 * fraction7^2 - fraction7 * fraction)
+	return 24 * epsilon * d * (2 * fraction7^2 - fraction7 * fraction)
 end
 
 
@@ -178,6 +178,8 @@ function verlet(parts, oldparts, timestep)
 
     num = length(parts)
     newparts = deepcopy(parts)
+    dt2 = timestep^2
+    itdt = 0.5 / timestep
     
     # Keep track of energy conversion
     kinetic = 0.0
@@ -198,7 +200,7 @@ function verlet(parts, oldparts, timestep)
                 dist = sqrt(dx^2 + dy^2 + dz^2)
 
                 # Use mixing rules
-                sigma = (parts[i].sigma + parts[j].sigma) / 2.0
+                sigma = (parts[i].sigma + parts[j].sigma) * 0.5
                 epsilon = sqrt(parts[i].epsilon * parts[j].epsilon)
                 
                 # Compute the forces in each direction
@@ -207,7 +209,7 @@ function verlet(parts, oldparts, timestep)
                 forcez += lj_force(sigma, epsilon, dist, dz)
 
                 # Compute potential energy
-                potential += lj_potential(sigma, epsilon, dist) / 2.0
+                potential += lj_potential(sigma, epsilon, dist) * 0.5
             end
         end
         
@@ -216,10 +218,7 @@ function verlet(parts, oldparts, timestep)
         newparts[i].acceleration.y = forcey / parts[i].mass
         newparts[i].acceleration.z = forcez / parts[i].mass
         
-        # Compute the new positions
-        dt2 = timestep^2
-        tdt = 2.0 * timestep
-
+        # Compute updated positions at t + dt
         newparts[i].position.x = 2.0 * parts[i].position.x - oldparts[i].position.x + 
                                 newparts[i].acceleration.x * dt2
         
@@ -228,15 +227,15 @@ function verlet(parts, oldparts, timestep)
         
         newparts[i].position.z = 2.0 * parts[i].position.z - oldparts[i].position.z + 
                                 newparts[i].acceleration.z * dt2
-
-                                
+ 
         # Compute the new speeds
-        newparts[i].velocity.x = (newparts[i].position.x - oldparts[i].position.x) / tdt
-        newparts[i].velocity.y = (newparts[i].position.y - oldparts[i].position.y) / tdt
-        newparts[i].velocity.z = (newparts[i].position.z - oldparts[i].position.z) / tdt
+        newparts[i].velocity.x = (newparts[i].position.x - oldparts[i].position.x) * itdt
+        newparts[i].velocity.y = (newparts[i].position.y - oldparts[i].position.y) * itdt
+        newparts[i].velocity.z = (newparts[i].position.z - oldparts[i].position.z) * itdt
 
+        # Compute total kinetic energy
         kinetic += (newparts[i].velocity.x^2 + newparts[i].velocity.y^2 +
-            newparts[i].velocity.z^2) * newparts[i].mass / 2.0
+            newparts[i].velocity.z^2) * newparts[i].mass * 0.5
     end
     
     println("Potential: $potential, Kinetic: $kinetic, Total: $(potential+kinetic)")
@@ -245,12 +244,93 @@ function verlet(parts, oldparts, timestep)
 end
 
 
+function velocity_verlet(parts, timestep)
+    parts::Array{Particle}
+    timestep::Real
+
+    num = length(parts)
+    newparts = deepcopy(parts)
+    dt2 = timestep^2
+
+    # Keep track of energy conversion
+    kinetic = 0.0
+    potential = 0.0
+
+    # Update new positions
+    for i in 1:1:num
+        
+        # Update the position for t + dt
+        newparts[i].position.x = parts[i].position.x + parts[i].velocity.x * timestep +
+            0.5 * parts[i].acceleration.x * dt2
+
+        newparts[i].position.y = parts[i].position.y + parts[i].velocity.y * timestep +
+            0.5 * parts[i].acceleration.y * dt2
+        
+        newparts[i].position.z = parts[i].position.z + parts[i].velocity.z * timestep +
+            0.5 * parts[i].acceleration.z * dt2
+    end
+
+    # Loop over all particle pairs
+    for i in 1:1:num
+        forcex = 0.0
+        forcey = 0.0
+        forcez = 0.0
+        for j in 1:1:num    
+            if i != j
+
+                # Compute distance between particles
+                dx = newparts[i].position.x - newparts[j].position.x
+                dy = newparts[i].position.y - newparts[j].position.y
+                dz = newparts[i].position.z - newparts[j].position.z
+                dist = sqrt(dx^2 + dy^2 + dz^2)
+
+                # Use mixing rules
+                sigma = (newparts[i].sigma + newparts[j].sigma) * 0.5
+                epsilon = sqrt(newparts[i].epsilon * newparts[j].epsilon)
+                
+                # Compute the forces in each direction
+                forcex += lj_force(sigma, epsilon, dist, dx)
+                forcey += lj_force(sigma, epsilon, dist, dy)
+                forcez += lj_force(sigma, epsilon, dist, dz)
+
+                # Compute total potential energy
+                potential += lj_potential(sigma, epsilon, dist) * 0.5
+            end
+        end
+        
+        # Compute the new acceleartions
+        newparts[i].acceleration.x = forcex / newparts[i].mass
+        newparts[i].acceleration.y = forcey / newparts[i].mass
+        newparts[i].acceleration.z = forcez / newparts[i].mass
+        
+        # Compute the new velocities
+        newparts[i].velocity.x = parts[i].velocity.x + 0.5 * (parts[i].acceleration.x +
+            newparts[i].acceleration.x) * timestep
+
+        newparts[i].velocity.y = parts[i].velocity.y + 0.5 * (parts[i].acceleration.y +
+            newparts[i].acceleration.y) * timestep
+
+        newparts[i].velocity.z = parts[i].velocity.z + 0.5 * (parts[i].acceleration.z +
+            newparts[i].acceleration.z) * timestep
+        
+        # Compute total kinetic energy
+        kinetic += (newparts[i].velocity.x^2 + newparts[i].velocity.y^2 +
+            newparts[i].velocity.z^2) * newparts[i].mass * 0.5
+    end
+    
+    println("Potential: $potential, Kinetic: $kinetic, Total: $(potential+kinetic)")
+
+    return newparts
+end
+
+
+
 function main()
 	box = Box(10, 10, 10)
     timestep = 0.01
     start = 0
     stop = 3 
-    num = 3
+    num = 300
 
 	particles = generate_particles(num, sigma=1.0, epsilon=4.0, mass=1.0, box=box)
     
@@ -260,12 +340,15 @@ function main()
     for _ in start:timestep:stop
         
         # Apply Verlet integration
-        newparticles, particles = verlet(newparticles, particles, timestep)
+        # newparticles, particles = verlet(newparticles, particles, timestep)
+        
+        # Apply Velocity Verlet integrator
+        newparticles = velocity_verlet(newparticles, timestep)
         
         # Print the results in .xyz format
         write(file, "$num \n")
         write(file, "\n")
-        for (_, val) in enumerate(particles)
+        for (_, val) in enumerate(newparticles)
             posx = Float32(val.position.x)
             posy = Float32(val.position.y)
             posz = Float32(val.position.z)
